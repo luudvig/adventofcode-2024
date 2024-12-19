bf045e5c06bccf16a0e223031ccb27fbad037f66
luudvig <ludvigolsson@outlook.com>
2024-12-19 15:37:13 +0100

    refactor part1

--- day9/part1.py
+++ day9/part1.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python3
 
 with open('input') as f:
-    map = [int(d) for d in f.read().rstrip()]
+    blocks, map = [], tuple(int(d) for d in f.read().rstrip())
 
-new_map = []
-
-i2 = 0
-for i, d in enumerate(map):
+i, j = 0, 0
+while i < len(map):
     if i % 2 == 0:
-        new_map.extend([i2] * d)
-        i2 += 1
+        blocks.extend([j] * map[i])
+        j += 1
     else:
-        new_map.extend(['.'] * d)
-
-
-i = 0
-while i < len(new_map):
-    if new_map[i] == '.':
-        e = new_map.pop()
-        while e == '.':
-             e = new_map.pop()
-        try:
-            new_map[i] = e
-        except IndexError:
-            new_map.append(e)
+        blocks.extend(['.'] * map[i])
     i += 1
 
-sum = 0
-for i, d in enumerate(new_map):
-    sum += i * d
+i, e = -1, None
+while i < len(blocks) - 1:
+    i += 1
+    if blocks[i] != '.':
+        continue
+    while True:
+        e = blocks.pop()
+        if e != '.':
+            break
+    if i < len(blocks):
+        blocks.pop(i)
+    blocks.insert(i, e)
 
-print(sum)
+checksum = tuple(i * d for i, d in enumerate(blocks) if d != '.')
+
+print(sum(checksum))
