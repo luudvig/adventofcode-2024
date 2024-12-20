3d880a45b61c93a006a22e28f74ea3ab94cdbe00
luudvig <ludvigolsson@outlook.com>
2024-12-20 17:12:40 +0100

    snapshot part2

--- day9/part2.py
+++ day9/part2.py
@@ -1,54 +1,57 @@
 #!/usr/bin/env python3
 
 with open('input') as f:
-    blocks, map = [], tuple(int(d) for d in f.read().rstrip())
+    blocks, map = '', tuple(int(d) for d in f.read().rstrip())
 
 i, j = -1, 0
 while i < len(map) - 1:
     i += 1
-    if not map[i]:
+    if map[i] == '0':
         continue
     elif i % 2 == 0:
-        blocks.append(str(j) * map[i])
+        blocks += str(j) * map[i]
         j += 1
     else:
-        blocks.append('.' * map[i])
+        blocks += '.' * map[i]
 
 i = len(blocks)
 while i > 0:
     i -= 1
-
-    if blocks[i][0] == '.':
+    
+    if blocks[i] == '.':
         continue
 
+    b1_end = i + 1
+    try:
+        b1_beg = next(k for k in range(i, -1, -1) if blocks[k] != blocks[i]) + 1
+    except StopIteration:
+        b1_beg = 0
+
+    i = b1_beg
+
     j = -1
-    while j < i - 1:
+    while j < i:
         j += 1
-        if blocks[j][0] != '.' or len(blocks[j]) < len(blocks[i]):
+
+        if blocks[j] != '.':
             continue
-        b = blocks.pop(i)
 
-        if i > 0 and blocks[i - 1][0] == '.' and i < len(blocks) and blocks[i][0] == '.':
-            blocks[i - 1] = blocks[i - 1] + '.' * len(b) + blocks[i]
-            blocks.pop(i)
-        elif i > 0 and blocks[i - 1][0] == '.':
-            blocks[i - 1] = blocks[i - 1] + '.' * len(b)
-        elif i < len(blocks) and blocks[i][0] == '.':
-            blocks[i] = blocks[i] + '.' * len(b)
-        else:
-            blocks.insert(i, '.' * len(b))
+        b2_beg = j
+        try:
+            b2_end = next(k for k in range(j, i) if blocks[k] != blocks[j])
+        except StopIteration:
+            b2_end = b1_beg
 
-        if len(blocks[j]) == len(b):
-            blocks.pop(j)
-        else:
-            blocks[j] = blocks[j][len(b):]
-        blocks.insert(j, b)
+        j = b2_end
 
-        break
+        if (d1 := b1_end - b1_beg) <= (d2 := b2_end - b2_beg):
+            blocks = blocks[:b2_beg] + blocks[b1_beg:b1_end] + blocks[b2_end - (d2 - d1):]
+            blocks = blocks[:b1_beg] + '.' * d1 + blocks[b1_end:]
+            break
 
 sum = 0
-for i, b in enumerate(''.join(blocks)):
+for i, b in enumerate(blocks):
     if b != '.':
-        sum += i * int(b)
+        sum += int(b) * i
 
 print(sum)
